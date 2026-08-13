0c2094039f36976ad927369adf17aee77865e923
ammaarun <ammaarunkumar2003@gmail.com>
2026-08-14 00:18:28 +0530

    Added spring boot skeleton

---
+++ src/test/java/com/shadowbase/ShadowbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
